602bc2a01b3498fe0e2c28c2cc9fbe618e011226
NicolasCharvieux <n.charvieux@it-students.com>
2019-11-26 15:22:07 +0000

    index modif

---
+++ traitement.php
@@ -0,0 +1,32 @@
+<?php
+
+$result = 0;
+
+switch ($_POST['sign']) {
+    case 'plus':
+        $result = (intval($_POST['val1']) + intval($_POST['val2']));
+        break;
+    case 'moins':
+        $result = (intval($_POST['val1']) - intval($_POST['val2']));
+        break;
+    case 'fois':
+        $result = (intval($_POST['val1']) * intval($_POST['val2']));
+        break;
+    case 'divise':
+        $result = (intval($_POST['val1']) / intval($_POST['val2']));
+        break;
+    case 'exposant':
+        $result = exposant(intval($_POST['val1']), intval($_POST['val2']));
+        break;
+    default:
+        echo 'WTF !?!';
+        break;
+}
+
+echo $_POST['val1'].' '.$_POST['sign'].' '.$_POST['val2']. ' = '. $result;
+
+$val1 = $_POST['val1'];
+$val2 = $_POST['val2'];
+$sign = $_POST['sign'];
+
+?>
