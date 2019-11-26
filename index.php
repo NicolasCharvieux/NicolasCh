9a71421de1ab55f875babee29e0ce6ed7875219f
NicolasCharvieux <n.charvieux@it-students.com>
2019-11-26 09:54:09 +0000

    Ajout

--- index.php
+++ index.php
@@ -6,12 +6,27 @@ if(!empty($_POST)){
 
     if($_POST['sign'] === 'plus') {
         echo(intval($_POST['val1']) + intval($_POST['val2']));
+
     } elseif($_POST['sign'] === 'moins') {
         echo(intval($_POST['val1']) - intval($_POST['val2']));
+
     } elseif($_POST['sign'] === 'fois') {
         echo(intval($_POST['val1']) * intval($_POST['val2']));
+
     } elseif($_POST['sign'] === 'divise') {
         echo(intval($_POST['val1']) / intval($_POST['val2']));
+
+    } elseif($_POST['sign'] === 'exposant') {
+        //boucle
+        $val1 = intval($_POST['val1']);
+        $val2 = intval($_POST['val2']);
+        $resultat = 1 ;
+        for($i = 0; $i<$val2; $i++) {
+            $resultat *= $val1;
+            // $resultat = $resultat * $val1 ;
+        }
+        echo $resultat
+
     } else {
         echo 'WTF !?!';
     }
@@ -26,6 +41,7 @@ if(!empty($_POST)){
         <option>moins</option>
         <option>fois</option>
         <option>divise</option>
+        <option>exposant</option>
     </select>
     <label for="val2">Val 2 </label>
     <input type="text" name="val2" id="val2" />
